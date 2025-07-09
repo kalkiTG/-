6a73212c39201f7bd4baf739606ca64ccffeef0f
ᛕꪖꪶᛕﺃ ༗ <surajkambozdhot@gmail.com>
2025-07-09 06:11:01 +0530

    Update main.py

--- main.py
+++ main.py
@@ -826,9 +826,9 @@ async def txt_handler(bot: Client, m: Message):
                 cmd = f'yt-dlp -f "{ytf}" "{url}" -o "{name}.mp4"'
 
             try:
-                cc = f'[🎥]Vid Id : {str(count).zfill(3)}\n**Video Title :** `{name1} [{res}p].mkv`\n<blockquote><b>Batch Name :</b> {b_name}</blockquote>\n\n**Extracted by➤**{CR}\n'
-                cc1 = f'[📕]Pdf Id : {str(count).zfill(3)}\n**File Title :** `{name1}.pdf`\n<blockquote><b>Batch Name :</b> {b_name}</blockquote>\n\n**Extracted by➤**{CR}\n'
-                cczip = f'[📁]Zip Id : {str(count).zfill(3)}\n**Zip Title :** `{name1}.zip`\n<blockquote><b>Batch Name :</b> {b_name}</blockquote>\n\n**Extracted by➤**{CR}\n' 
+                cc = f'🪐Vid Id : {str(count).zfill(3)}\n\n**📝 Title :** `{name1} `\n\n<blockquote><b>📚Batch Name :</b> {b_name}</blockquote>\n\n**Downloded By-**<blockquote>{CR}</blockquote>\n'
+                cc1 = f'🪐Pdf Id : {str(count).zfill(3)}\n\n**📝 Title :** `{name1} `\n\n<blockquote><b>📚Batch Name :</b> {b_name}</blockquote>\n\n**Downloaded By-**<blockquote>{CR}</blockquote>\n'
+                cczip = f'🪐Zip Id : {str(count).zfill(3)}\n\n**📝 Title :** `{name1} `\n\n<blockquote><b>📚Batch Name :</b> {b_name}</blockquote>\n\n**Downloaded By-**<blockquote>{CR}</blockquote>\n' 
                 ccimg = f'[🖼️]Img Id : {str(count).zfill(3)}\n**Img Title :** `{name1}.jpg`\n<blockquote><b>Batch Name :</b> {b_name}</blockquote>\n\n**Extracted by➤**{CR}\n'
                 ccm = f'[🎵]Audio Id : {str(count).zfill(3)}\n**Audio Title :** `{name1}.mp3`\n<blockquote><b>Batch Name :</b> {b_name}</blockquote>\n\n**Extracted by➤**{CR}\n'
                 cchtml = f'[🌐]Html Id : {str(count).zfill(3)}\n**Html Title :** `{name1}.html`\n<blockquote><b>Batch Name :</b> {b_name}</blockquote>\n\n**Extracted by➤**{CR}\n'
